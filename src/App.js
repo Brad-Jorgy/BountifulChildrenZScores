5306dff747e75cb0226f5ecd58e4b4a9396f7d24
Brad Jorgensen <brad.taylor.jorgensen@gmail.com>
2020-08-10 23:23:38 -0600

    good to go

--- src/App.js
+++ src/App.js
@@ -6,17 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://nutrition.liahonakids.org/children/countries/true"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Future page of Bountiful Children Z Scores
+
         </a>
       </header>
     </div>
